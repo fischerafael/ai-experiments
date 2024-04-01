59e25a379629a30d4fcd3c99ecb5b8265d5cb425
Rafael Fischer <rafaelsanfischer@gmail.com>
2024-04-01 18:14:53 -0300

    feat: working with texts

--- pages/api/tts.ts
+++ pages/api/tts.ts
@@ -7,14 +7,22 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log("[text]", req.body.text);
-  const file = await textToSpeech(req.body.text);
+  try {
+    console.log("[text]", req.body.text);
+    const file = await textToSpeech(req.body.text);
 
-  res.setHeader("Content-Type", "audio/mpeg");
-  res.setHeader(
-    "Content-Disposition",
-    `attachment; filename=${DEFAULT_NAME}-tts.mp3`
-  );
+    console.log("[file]", file);
 
-  return res.send(file);
+    res.setHeader("Content-Type", "audio/mpeg");
+    res.setHeader(
+      "Content-Disposition",
+      `attachment; filename=${DEFAULT_NAME}-tts.mp3`
+    );
+
+    return res.status(201).send(file);
+  } catch (e: any) {
+    return res.status(500).json({
+      error: e.message,
+    });
+  }
 }
